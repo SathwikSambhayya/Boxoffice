991452f349dfae141fab8f39e70525d79f8535bd
Satwik_S <sathwiksambhayya@gmail.com>
2021-09-20 15:34:28 +0530

    gamehistory

---
+++ Desktop/internshala/Tictac/tictactoegame/src/components/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Board from "./Board";
+import Gamelogic from "./Gamelogic";
+function App() {
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isnext: true },
+  ]);
+  const [currentuser, setcurrentuser] = useState(0);
+  const current = history[currentuser];
+
+  const winner = Gamelogic(current.board);
+  const msg = winner
+    ? `Winner is ${winner}`
+    : current.isnext
+    ? `Next player is X`
+    : `Next player is O`;
+
+  function handleClick(position) {
+    if (current.board[position] || winner) return;
+    setHistory((prev) => {
+      const last = prev[prev.length - 1];
+      const newBoard = last.board.map((square, pos) => {
+        if (pos === position) return last.isnext ? "X" : "O";
+        return square;
+      });
+      return [...prev, { board: newBoard, isnext: !last.isnext }];
+    });
+    setcurrentuser((prev) => prev + 1);
+  }
+
+  return (
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
+      <h2>{msg}</h2>
+
+      <Board board={current.board} handleClick={handleClick} />
+    </div>
+  );
+}
+
+export default App;
